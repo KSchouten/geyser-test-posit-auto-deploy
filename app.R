117e5e4ee064b11c05da54a14ea8ddf1cf8a3b23
Kim Schouten <kschouten@nza.nl>
2023-08-02 18:03:57 +0200

    Test change in source

--- app.R
+++ app.R
@@ -13,7 +13,7 @@ library(shiny)
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Old Faithful Geyser Data - Git auto deploy test for Posit Connect"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
